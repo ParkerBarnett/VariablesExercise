97671881d31fa700358a64506b94e2e795058c82
Parker Barnett <parkerbarnett02@gmail.com>
2023-03-16 10:18:10 -0400

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1
+// const
+
+// 2
+let num = 20;
+console.log(num);
+
+// 3
+num = 15;
+console.log(num);
+
+// 4
+num = num + 7;
+console.log(num);
+
+// 5
+num--;
+console.log(num);
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7
+let blank;
+console.log(num);
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+console.log(blank);
+
+// BONUS
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+
+// 2
+console.log(quarter, dine, nickel, penny);
+
+// 3
+let letters = "qwertyuiop"
+
+// 4
+// let pSpot = "qwertyuiop".length;
+// pSpot--;
+// console.log(letters[pSpot]);
+// OR
+console.log(letters[letters.length - 1]);
+
